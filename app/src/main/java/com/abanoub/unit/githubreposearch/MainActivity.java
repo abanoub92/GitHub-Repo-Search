dd8714db00b0eb5abd77f0b86012a8dc503f4563
abanoub ghebreal <abanoub.eskander15@gmail.com>
2018-08-02 23:05:28 +0200

    Step NO.3: add some polish to the app

--- app/src/main/java/com/abanoub/unit/githubreposearch/MainActivity.java
+++ app/src/main/java/com/abanoub/unit/githubreposearch/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
     /** TextView for display the search result */
     private TextView mSearchResultsTextView;
 
+    /** TextView for display the error message if loading data is failed */
+    private TextView mErrorMessageDisplay;
+
+    /** ProgressBar for display an indicator while data is display */
+    private ProgressBar mLoadingIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +49,42 @@ public class MainActivity extends AppCompatActivity {
 
         /* reference by id to search result TextView xml */
         mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json);
+
+        /* reference by id to error message TextView xml */
+        mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
+
+        /* reference by id to progress bar xml */
+        mLoadingIndicator = findViewById(R.id.pb_loading_indicator);
+    }
+
+    /**
+     * This method will make the View for the JSON data visible and
+     * hide the error message.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showJsonDataView(){
+        // First, make sure the error is invisible
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+
+        // Then, make sure the JSON data is visible
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * This method will make the error message visible and hide the JSON
+     * View.
+     * <p>
+     * Since it is okay to redundantly set the visibility of a View, we don't
+     * need to check whether each view is currently visible or invisible.
+     */
+    private void showErrorMessage(){
+        // First, hide the currently visible data
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+
+        // Then, show the error
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
     }
 
 
@@ -77,22 +121,40 @@ public class MainActivity extends AppCompatActivity {
     private class GithubQueryTask extends AsyncTask<URL, Void, String>{
 
         @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // show the indicator while the data is fetching
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected String doInBackground(URL... urls) {
             URL url = urls[0];
-            String result = "";
+            String result = null;
             try {
+                // fetching the data from the internet and extract it from JSON
                 result = NetworkUtils.getResponseFromHttpUrl(url);
             } catch (IOException e) {
                 Log.e(LOG_TAG, "An error while getting the JSON data", e);
             }
+            // return the result data
             return result;
         }
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            if (s != null)
+            // Hide the indicator when the data is fetch
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+            if (s != null && !s.equals("")) {
+                //display the mSearchResultsTextView to display the data
+                showJsonDataView();
+
+                // adding the data to text view to show it
                 mSearchResultsTextView.setText(s);
+            }else
+                // if the data is null display the error message
+                showErrorMessage();
         }
     }
 }
